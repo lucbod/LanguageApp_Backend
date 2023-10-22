be88fa0a32dc56c19da8158cd5f98692d78f4099
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2023-10-22 12:25:08 +0200

    Messages are being saved into db

--- src/main/java/com/example/LanguageAppMongoDb/controller/chatServer/ChatController.java
+++ src/main/java/com/example/LanguageAppMongoDb/controller/chatServer/ChatController.java
@@ -1,5 +1,6 @@
 package com.example.LanguageAppMongoDb.controller.chatServer;
 
+import com.example.LanguageAppMongoDb.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -11,18 +12,30 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final MessageRepository messageRepository;
+
 
     @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
+    public ChatController(SimpMessagingTemplate simpMessagingTemplate, MessageRepository messageRepository) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.messageRepository = messageRepository;
+    }
 
     @MessageMapping("/message")
     @SendTo("/chatroom/public")
-    private com.example.LanguageAppMongoDb.model.chatServer.Message receivePublicMessage(@Payload com.example.LanguageAppMongoDb.model.chatServer.Message message){
+    private com.example.LanguageAppMongoDb.model.chatServer.Message receivePublicMessage
+            (@Payload com.example.LanguageAppMongoDb.model.chatServer.Message message){
+        messageRepository.save(message);
         return message;
     }
 
     @MessageMapping("/private-message")
-    public com.example.LanguageAppMongoDb.model.chatServer.Message receivePrivateMessage(@Payload com.example.LanguageAppMongoDb.model.chatServer.Message message){
+    public com.example.LanguageAppMongoDb.model.chatServer.Message receivePrivateMessage
+            (@Payload com.example.LanguageAppMongoDb.model.chatServer.Message message){
+
+        messageRepository.save(message);
+
         simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private", message); // /user/David/private
         return message;
     }
