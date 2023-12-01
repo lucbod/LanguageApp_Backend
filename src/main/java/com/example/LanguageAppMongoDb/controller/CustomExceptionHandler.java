79228e9409d8ad58cbe1128f449d2dc467f8f5f0
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2023-12-01 15:49:22 +0100

    500 to 403 changed

--- src/main/java/com/example/LanguageAppMongoDb/controller/CustomExceptionHandler.java
+++ src/main/java/com/example/LanguageAppMongoDb/controller/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.LanguageAppMongoDb.controller;
 import com.example.LanguageAppMongoDb.auth.CustomAuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +15,14 @@ public class CustomExceptionHandler {
     @ExceptionHandler(CustomAuthenticationException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ResponseEntity<String> handleCustomAuthenticationException(CustomAuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(e.getMessage());
+    }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
                 .body(e.getMessage());
     }
