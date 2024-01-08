6d2ac32bd7ae500182920aa828b96c1dd8f594e1
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2024-01-08 13:34:14 +0100

    fixing workflow1

--- src/test/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplicationTests.java
+++ src/test/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplicationTests.java
@@ -1,16 +1,19 @@
 package com.example.LanguageAppMongoDb;
 
+import com.example.LanguageAppMongoDb.config.JwtAuthenticationFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.bson.assertions.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class LanguageAppMongoDbApplicationTests {
-
+	JwtAuthenticationFilter jwtAuthenticationFilter;
 	@Test
 	void contextLoads() {
-		assertTrue(true);
+
+		assertNotNull(jwtAuthenticationFilter);
 	}
 
 }
