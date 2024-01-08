5ea89100af8ff0aedfab6b7d7b79058e86002b26
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2024-01-08 13:26:39 +0100

    fixing workflow

--- src/test/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplicationTests.java
+++ src/test/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.LanguageAppMongoDb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class LanguageAppMongoDbApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
