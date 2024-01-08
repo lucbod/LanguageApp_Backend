815305de25cd6349497e13d3ee1fb5e9f74fb43f
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2024-01-08 13:52:53 +0100

    fixing workflow2,@Autowired was missing

--- src/test/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplicationTests.java
+++ src/test/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplicationTests.java
@@ -2,13 +2,15 @@ package com.example.LanguageAppMongoDb;
 
 import com.example.LanguageAppMongoDb.config.JwtAuthenticationFilter;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.bson.assertions.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @SpringBootTest
 class LanguageAppMongoDbApplicationTests {
+	@Autowired
 	JwtAuthenticationFilter jwtAuthenticationFilter;
 	@Test
 	void contextLoads() {
