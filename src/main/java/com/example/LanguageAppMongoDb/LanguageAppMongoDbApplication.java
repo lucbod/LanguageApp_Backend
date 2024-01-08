e13795dfc843deeda063f3b5cebe5de18b9c066e
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2024-01-08 19:14:05 +0100

    fixing mismatch between local code and pipeline

--- src/main/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplication.java
+++ src/main/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplication.java
@@ -2,9 +2,10 @@ package com.example.LanguageAppMongoDb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+@ComponentScan(basePackages = {"com.example.LanguageAppMongoDb", "com.example.LanguageAppMongoDb.testEndpointForUsersController"})
 public class LanguageAppMongoDbApplication {
 
 	public static void main(String[] args) {
