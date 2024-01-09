d6c4a8008797c623ec72f769f3940402b65a9336
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2024-01-09 18:09:35 +0100

    ComponentScan removed

--- src/main/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplication.java
+++ src/main/java/com/example/LanguageAppMongoDb/LanguageAppMongoDbApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.LanguageAppMongoDb", "com.example.LanguageAppMongoDb.testEndpointForUsersController"})
 public class LanguageAppMongoDbApplication {
 
 	public static void main(String[] args) {
