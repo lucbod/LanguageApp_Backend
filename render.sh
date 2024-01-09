d8ec4e0c1f2e0d63ec9e6e9243d1d6704a87ca0d
Lucia Bodnarova <lucia.bodnarova@kosickaakademia.sk>
2024-01-09 14:27:19 +0100

    fixing mismatch between local code and pipeline7

--- render.sh
+++ render.sh
@@ -4,7 +4,7 @@
 SERVICE_NAME="Spring Boot LanguageApp"
 
 # Build your project (replace the build command with your actual build command)
-./mvnw clean install
+mvn clean install
 
 # Deploy to Render using deploy hook
 render deploy-hook --from ./target/LanguageAppMongoDb-0.0.1-SNAPSHOT.jar --config render.yaml $SERVICE_NAME
